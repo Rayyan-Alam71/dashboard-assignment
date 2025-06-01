9bee5778c2a74a6d3fa51aa9468690dad96b7858
Rayyan-Alam71 <rayyanalam752@gmail.com>
2025-06-01 14:19:43 +0530

    final commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 "use client"
 import Header from "@/components/Header";
 import Skeleton from "@/components/Skeleton";
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
 import {  useRouter } from "next/navigation"
 import { useEffect } from "react";
 
 export default function Home() {
-  const router = useRouter();
+  const router : AppRouterInstance = useRouter();
   
   useEffect(()=>{
     router.push("/dashboard")
